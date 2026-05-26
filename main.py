e4842fffb61c073dbd31238ddc20c77209f18da6
umacpro <umeshp203@gmail.com>
2026-05-25 19:30:56 -0700

    search agents

--- main.py
+++ main.py
@@ -12,19 +12,32 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
+
 # llm = ChatOpenAI(model="gpt-5")
 llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-3.1-flash-lite",
         temperature=0,
         max_retries=2
     )
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse.model_json_schema())
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="search for 3 SDET job for ai engineer which involve GenAI test automation using lanchain in bay area, California and list their details")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 SDET job which involve GenAI test automation in bay area, California or remote location and list their details")})
     print(result)
 
 if __name__ == "__main__":
